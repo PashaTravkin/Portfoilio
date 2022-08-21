8f0655dd06845b33934741b21602df47c58dcb35
PashaTravkin <refueller2022@bk.ru>
2022-08-21 19:55:44 +0300

    Started the portfolio

--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import s from './Skills.module.css'
+import Skill from "./skill/Skill";
 
 const Skills = () => {
     return (
         <div className={s.skills}>
-
+            <div className={s.container}>
+                <h2 className={s.title}>My skills</h2>
+                <div className={s.containerForSkills}>
+                    <Skill title={'React'} description={'Lorem ipsum dolor sit amet.'}/>
+                    <Skill title={'Redux'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing.'}/>
+                    <Skill title={'JS'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti!'}/>
+                </div>
+            </div>
         </div>
     );
 };
